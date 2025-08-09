7494cb4d76ec621817030cd19ea1aea44335210f
VWRT <famkuokviet@gmail.com>
2025-08-09 15:49:56 +0700

    Update setup_dashboard.sh

--- setup_dashboard.sh
+++ setup_dashboard.sh
@@ -4,6 +4,7 @@ REPO_URL="https://github.com/vietter99/vwrt-dashboard.git"
 DEST_DIR="/www/vwrt"
 UHTTPD_CONF="/etc/config/uhttpd"
 
+# --- Tải dự án ---
 rm -rf "$DEST_DIR"
 git clone "$REPO_URL" "$DEST_DIR"
 
@@ -11,11 +12,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+# --- Cấp quyền ---
 BACKEND_SCRIPT="$DEST_DIR/cgi-bin/backend.lua"
 if [ -f "$BACKEND_SCRIPT" ]; then
     chmod +x "$BACKEND_SCRIPT"
 fi
 
+# --- Cấu hình Web Server ---
 cp "$UHTTPD_CONF" "$UHTTPD_CONF.bak"
 
 sed -i "s/.*list index_page 'index.html'.*/#&/" "$UHTTPD_CONF"
@@ -31,6 +34,7 @@ if ! grep -q "list interpreter '.lua=/usr/bin/lua'" "$UHTTPD_CONF"; then
 	list interpreter '.lua=/usr/bin/lua'" "$UHTTPD_CONF"
 fi
 
+# --- Khởi động lại dịch vụ ---
 /etc/init.d/uhttpd restart
 
 exit 0
